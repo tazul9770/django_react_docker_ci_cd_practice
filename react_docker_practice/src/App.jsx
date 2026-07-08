bf355370db788e8b08b3f23b3c4d4d7af1d79f61
Tazul Islam <linkedin52425@gmail.com>
2026-07-08 16:35:42 +0600

    CI CD summary

--- react_docker_practice/src/App.jsx
+++ react_docker_practice/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count {count}
+          Count kor {count}
         </button>
       </section>
 
