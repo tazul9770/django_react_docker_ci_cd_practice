0321de7c553fa53bbe7ffbf59f73afb86abb80c1
Tazul Islam <linkedin52425@gmail.com>
2026-07-08 16:38:44 +0600

    django project some changes

--- practice_django/demo/views.py
+++ practice_django/demo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def test(request):
-    return HttpResponse("Django test view updated done")
+    return HttpResponse("Docker and ci cd pipeline done")
